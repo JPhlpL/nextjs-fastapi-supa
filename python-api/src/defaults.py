4ef242f36c0ee3254a2e1166cc70115602f44086
John Philip <johnphilipomamalin1206@gmail.com>
2025-03-10 21:31:19 +0800

    added nextjsfastapisupa workspace on modal

--- python-api/src/defaults.py
+++ python-api/src/defaults.py
@@ -1,4 +1,4 @@
-SECRETS_NAME = "DEVELOPMENT"
+SECRETS_NAME = "nextjsfastapisupa-secrets"
 API_STUB_NAME = "nextjsfastapisupa-api"
-MODAL_PRODUCTION_ENVIRONMENT = "dev_nextjsfastapisupa"
-MODAL_DEVELOPMENT_ENVIRONMENT = "dev_nextjsfastapisupa"
+MODAL_PRODUCTION_ENVIRONMENT = "production"
+MODAL_DEVELOPMENT_ENVIRONMENT = "development"
