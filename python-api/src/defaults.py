b4888be997df760375c21295cb466bdbc8c03d9b
John Philip <johnphilipomamalin1206@gmail.com>
2025-03-10 19:58:18 +0800

    modal container ready, yes!

---
+++ python-api/src/defaults.py
@@ -0,0 +1,4 @@
+SECRETS_NAME = "DEVELOPMENT"
+API_STUB_NAME = "nextjsfastapisupa-api"
+MODAL_PRODUCTION_ENVIRONMENT = "dev_nextjsfastapisupa"
+MODAL_DEVELOPMENT_ENVIRONMENT = "dev_nextjsfastapisupa"
--- python-api/src/main.py
+++ python-api/src/main.py
@@ -1,32 +1,46 @@
-from fastapi import FastAPI
-from fastapi.middleware.cors import CORSMiddleware
-from src.utils.logger import setup_logfire
-from src.routers import (
-    users
-) # Import the user router from the router directory
+from modal import App, asgi_app, Secret
+import os
+from src.defaults import (
+    API_STUB_NAME,
+    MODAL_PRODUCTION_ENVIRONMENT,
+    MODAL_DEVELOPMENT_ENVIRONMENT,
+    SECRETS_NAME
+)
+from src.images import api_image  # import the API image from images.py
 
-# Initialize FastAPI and call it 'app'
-app = FastAPI()
-
-setup_logfire(app)
-
-# Allow CORS for specific origins
-origins = [
-    "http://localhost:3000",  # Your frontend URL
-    "http://10.73.130.177:3000",  # Your frontend URL
-    "http://10.74.12.26:3000",  # Your frontend URL
-    "http://172.24.68.121:3000"  # Your frontend URL
-    # Add other origins as needed
-]
-
-app.add_middleware(
-    CORSMiddleware,
-    allow_origins=["*"],  # Allows specific origins
-    # allow_origins=origins,  # Allows specific origins
-    allow_credentials=True,
-    allow_methods=["*"],  # Allows all methods (GET, POST, OPTIONS, etc.)
-    allow_headers=["*"],  # Allows all headers
+MODAL_ENVIRONMENT = os.environ.get("MODAL_ENVIRONMENT")
+is_production = MODAL_ENVIRONMENT == MODAL_PRODUCTION_ENVIRONMENT
+# For local, we still point to development secrets so we don't need to manage specific secrets
+secrets_env = (
+    MODAL_PRODUCTION_ENVIRONMENT if is_production else MODAL_DEVELOPMENT_ENVIRONMENT
 )
 
-# Include the user router
-app.include_router(users.router)
+# Create a Modal App instance.
+app = App(name=API_STUB_NAME)  # type: ignore
+
+# Wrap your FastAPI app in a Modal function.
+@app.function(
+    image=api_image,
+    secrets=[Secret.from_name(SECRETS_NAME, environment_name=secrets_env)],
+    # cloud="oci",  # aws, gcp, oci, auto. We choose oci for better latency with db, as we
+    # allow_concurrent_inputs=1000,
+    # region=("us-east"),
+    # timeout=60 * 60,  # set an appropriate timeout (here, 1 hour)
+)
+@asgi_app()
+def fastapi_app():
+    from src.api import web_app
+    from src.utils.logger import setup_logger
+
+    logger = setup_logger()
+    logger.info(
+        f"FastAPI app started"
+    )
+
+    return web_app
+
+# local entrypoints for testing
+@app.local_entrypoint()  # type: ignore
+def main() -> None:
+
+    return None
